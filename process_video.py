d50bb0c943965d02c46f3f3801878518b46f821d
kun <24101467d@connect.polyu.hk>
2025-04-12 20:01:48 +0800

    加入并适当修改了dehaze，同时对process做出调整了

--- process_video.py
+++ process_video.py
@@ -8,6 +8,8 @@ from PIL import Image
 from fusion import laplacian_pyramid_fusion
 from recognition import detect_humans
 from image_haze_removel import image_haze_removel
+from dehaze import DarkChannel, AtmLight, TransmissionEstimate, TransmissionRefine, Recover
+
 
 # 打开可见光和红外视频文件
 visible_video = cv2.VideoCapture('rgb.mp4')
@@ -35,6 +37,12 @@ model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
 
 kalman_filters = []
 
+print("请选择去烟算法:")
+print("1. 原有的去烟算法")
+print("2. dehaze.py 中的去烟算法")
+choice = input("请输入你的选择 (1 或 2): ")
+
+
 while True:
     # 读取可见光和红外视频的帧
     ret_visible, visible_frame = visible_video.read()
@@ -49,13 +57,24 @@ while True:
 
     # 进行拉普拉斯金字塔融合
     fused_frame = laplacian_pyramid_fusion(visible_frame, infrared_frame)
-
+    if choice == '1':
     # 去烟处理
-    pil_image = Image.fromarray(cv2.cvtColor(fused_frame, cv2.COLOR_BGR2RGB))
-    dehaze_image = image_haze_removel(pil_image)
-    dehaze_image = dehaze_image.squeeze(0).permute(1, 2, 0).cpu().detach().numpy()
-    dehaze_image = np.clip(dehaze_image * 255, 0, 255).astype(np.uint8)
-    dehaze_frame = cv2.cvtColor(dehaze_image, cv2.COLOR_RGB2BGR)
+        pil_image = Image.fromarray(cv2.cvtColor(fused_frame, cv2.COLOR_BGR2RGB))
+        dehaze_image = image_haze_removel(pil_image)
+        dehaze_image = dehaze_image.squeeze(0).permute(1, 2, 0).cpu().detach().numpy()
+        dehaze_image = np.clip(dehaze_image * 255, 0, 255).astype(np.uint8)
+        dehaze_frame = cv2.cvtColor(dehaze_image, cv2.COLOR_RGB2BGR)
+    elif choice == '2':
+        I = fused_frame.astype('float64') / 255
+        dark = DarkChannel(I, 15)
+        A = AtmLight(I, dark)
+        te = TransmissionEstimate(I, A, 15)
+        t = TransmissionRefine(fused_frame, te)
+        dehaze_frame = Recover(I, t, A, 0.1)
+        dehaze_frame = np.clip(dehaze_frame * 255, 0, 255).astype(np.uint8)
+    else:
+        print("无效的选择，请重新运行程序并输入 1 或 2。")
+        break
 
     # 检测人体并标注
     annotated_frame, kalman_filters = detect_humans(dehaze_frame, model, kalman_filters)
